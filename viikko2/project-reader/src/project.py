d06dbebd504c790368b80a85f413d3e5c3b6ae29
Riku <134082872+rigozu9@users.noreply.github.com>
2024-11-06 17:33:51 +0200

    viikko 2 ex 2 done

--- viikko2/project-reader/src/project.py
+++ viikko2/project-reader/src/project.py
@@ -1,17 +1,24 @@
 class Project:
-    def __init__(self, name, description, dependencies, dev_dependencies):
+    def __init__(self, name, license, authors, description, dependencies, dev_dependencies):
         self.name = name
         self.description = description
+        self.license = license
+        self.authors = authors
         self.dependencies = dependencies
         self.dev_dependencies = dev_dependencies
 
     def _stringify_dependencies(self, dependencies):
-        return ", ".join(dependencies) if len(dependencies) > 0 else "-"
+        return "\n- " + "\n- ".join(dependencies) if len(dependencies) > 0 else "-"
 
     def __str__(self):
         return (
             f"Name: {self.name}"
             f"\nDescription: {self.description or '-'}"
+            f"\nLicense: {self.license}"
+            f"\n"
+            f"\nAuthors: {self._stringify_dependencies(self.authors)}"
+            f"\n"
             f"\nDependencies: {self._stringify_dependencies(self.dependencies)}"
+            f"\n"
             f"\nDevelopment dependencies: {self._stringify_dependencies(self.dev_dependencies)}"
         )
--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -1,5 +1,6 @@
 from urllib import request
 from project import Project
+import tomli
 
 
 class ProjectReader:
@@ -9,7 +10,17 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
+
+        toml_dict = tomli.loads(content)
+
+        navigated_toml_dict = toml_dict["tool"]["poetry"]
+
+        print(navigated_toml_dict)
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        return Project(navigated_toml_dict["name"],
+                       navigated_toml_dict["license"],
+                       navigated_toml_dict["authors"],
+                       navigated_toml_dict["description"],
+                       navigated_toml_dict["dependencies"],
+                       navigated_toml_dict["group"]["dev"]["dependencies"])
