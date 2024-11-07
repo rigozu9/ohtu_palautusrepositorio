3a4fdbe3a1cc4d25cea14a70325fb215194ed4b0
Riku Nummi <rikunummi9@gmail.com>
2024-11-07 18:18:38 +0200

    viikko 2 t. 11 done

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,26 @@
+import requests
 from player import Player
 
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
+    response = requests.get(url).json()
+
+    # print("JSON-muotoinen vastaus:")
+    # print(response)
+
+    players = []
+
+    for player_dict in response:
+        player = Player(player_dict)
+        if player.nationality == "FIN":
+            players.append(player)
+
+    # print("Oliot:")
+    # print(sorted(players, key=lambda x: x.goals + x.assists, reverse=True))
+    sorted_players = sorted(players, key=lambda x: x.goals + x.assists, reverse=True)
+
+    for player in sorted_players:
+        print(player)
 
 if __name__ == "__main__":
     main()
