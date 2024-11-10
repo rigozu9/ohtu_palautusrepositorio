cad9849690215fb0b9941d8237a5386f39b7b928
Riku Nummi <rikunummi9@gmail.com>
2024-11-10 13:49:29 +0200

    pushed to get files from laptop to pc

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,5 +1,8 @@
 from player_reader import PlayerReader
 from player_stats import PlayerStats
+from rich import print
+from rich.prompt import Prompt
+
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
@@ -7,8 +10,14 @@ def main():
     stats = PlayerStats(reader)
     players = stats.top_scorers_by_nationality("FIN")
 
+    print("[italic]Nhl statistics by nationality[italic]")
+
+    seasons = ["2018-19", "2019-20", "2020-21", "2021-22", "2022-23", "2023-24", "2024-25"]
+
+    selected_season = Prompt.ask("Select season", choices=seasons)
+
     for player in players:
-        print(player)
+        print(f"[bold magenta]{player}[bold magenta]", ":vampire:")
 
 if __name__ == "__main__":
     main()
