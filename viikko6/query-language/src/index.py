e89ead5be53b2b3d7443b0b4833504222fb54b12
Riku <134082872+rigozu9@users.noreply.github.com>
2024-12-08 18:15:40 +0200

    viikko 6 teht 4 ja 5 done

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,29 +1,30 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, Not, All, HasFewerThan
+from query_builder import QueryBuilder
 
 def main():
-    url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players.txt"
+    url = "https://studies.cs.helsinki.fi//nhlstats/2023-24/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    matcher = And(
-        # HasAtLeast(5, "goals"),
-        # HasAtLeast(20, "assists"),
-        # PlaysIn("PHI"),
-        # Not(HasAtLeast(2, "goals")),
-        # PlaysIn("NYR"),
-        HasFewerThan(2, "goals"),
-        PlaysIn("NYR")
+    query = QueryBuilder()
+
+    matcher = (
+    query
+        .one_of(
+        query.plays_in("PHI")
+            .has_at_least(10, "assists")
+            .has_fewer_than(10, "goals")
+            .build(),
+        query.plays_in("EDM")
+            .has_at_least(50, "points")
+            .build()
+        )
+        .build()
     )
-    filtered_with_all = stats.matches(All())
-    print(len(filtered_with_all))
-
-
 
     for player in stats.matches(matcher):
         print(player)
-
 
 if __name__ == "__main__":
     main()
---
+++ viikko6/query-language/src/query_builder.py
@@ -0,0 +1,22 @@
+from matchers import And, HasAtLeast, PlaysIn, All, HasFewerThan, Or
+
+class QueryBuilder:
+    def __init__(self, matcher=All()):
+        self._matcher = matcher
+
+    def build(self):
+        return self._matcher
+
+    def plays_in(self, team):
+        return QueryBuilder(And(self._matcher, PlaysIn(team)))
+        
+    def has_at_least(self, value, attr):
+        return QueryBuilder(And(self._matcher, HasAtLeast(value, attr)))
+
+    def has_fewer_than(self, value, attr):
+        return QueryBuilder(And(self._matcher, HasFewerThan(value, attr)))
+
+    def one_of(self, *matchers):
+        return QueryBuilder(Or(*matchers))
+    
+
